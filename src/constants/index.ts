8f79c9492f7e791cddc00a410600ad7094163f79
Asilbek Egamnazarov <byasilbek9@gmail.com>
2026-07-23 09:08:17 +0500

    git add

--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const AI_MODEL = "gemini-flash-latest";
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { bot } from "./bot.js";
-import { registerBusinessHandler } from "./handlers/business-message.handler.js";
-
-registerBusinessHandler(bot);
-
-bot.start();
-
-console.log("🤖 AI Secretary started");
